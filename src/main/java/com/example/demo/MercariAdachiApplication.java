c7172f027c9d347c27dfe7f7f69ef00c25284b50
adachisota <sota.adachi@rakus-partners.co.jp>
2020-04-06 16:10:55 +0900

    first commit

---
+++ src/main/java/com/example/demo/MercariAdachiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercariAdachiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercariAdachiApplication.class, args);
+	}
+
+}
